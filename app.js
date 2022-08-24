c280858de17ce65f5d2f44ed2c570233be7cd499
diullyledur <diullyledur@gmail.com>
2022-08-24 18:16:33 +0000

    cria servidor

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.use(express.static(`${__dirname}/public`))
+
+app.get('/ola',(req,res) => {
+    res.send("ola mundo!")
+})
+
+server.listen(port, () => {
+  console.log(`Servidor ouvindo na porta ${port}`);
+});
