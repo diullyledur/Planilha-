0f58c344f04d435f8364216c15b4a27ff13e8f66
diullyledur <diullyledur@gmail.com>
2022-08-24 18:55:09 +0000

    aula do dia 24/08

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const TransacoesRepositorio = require('./transacoes-repositorio.js')
 const app = express()
-const port = 3000;
+const port = 3000
 
 app.use(express.static(`${__dirname}/public`))
 
-app.get('/ola',(req,res) => {
-    res.send("ola mundo!")
+app.get('/transacoes', (req, res) => {
+    const repositorio = new TransacoesRepositorio()
+    const transacoes = repositorio.listarTransacoes()
+    res.send(transacoes)
 })
 
-server.listen(port, () => {
-  console.log(`Servidor ouvindo na porta ${port}`);
-});
+app.get('/criar-transacao', (req, res) => {
+    const repositorio = new TransacoesRepositorio()
+    const transacao = {
+      valor: 10,
+      descricao: 'Pastel'
+    }
+    repositorio.criarTransacao(transacao)
+    res.status(201).send(transacao)
+  })
+
+app.listen(port, () => {
+  console.log(`Servidor ouvindo na porta ${port}`)
+})
